ae25b4d9d4c59201404eff194d57e54437b0c5ff
ki-hara <high.kihara@gmail.com>
2025-01-16 20:11:36 +0900

    리소스 추가 및 네비게이션 테스트

--- HappyCat/HappyCat/Assets/Script/TestScript.cs
+++ HappyCat/HappyCat/Assets/Script/TestScript.cs
@@ -1,25 +1,38 @@
 using HC.Data;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TestScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
     void Start()
     {
-        UGSManager.Init();
+        agent.speed += 0.1f;
 
-        foreach(var a in GuestTable.Guest.GuestList)
-        {
-            Debug.Log(a.code);
-            Debug.Log(a.icon);
-            Debug.Log(a.category);
-            Debug.Log(a.food.Count);
-            Debug.Log(a.food[0]);
-            Debug.Log(a.food[1]);
-            Debug.Log(a.food[2]);
-            Debug.Log(a.ani_eat01.Count);
-            Debug.Log("----------------");
-        }
+        agent.SetDestination(new Vector3(-0.77f, 11.58f));
+        //UGSManager.Init();
+
+        //foreach(var a in GuestTable.Guest.GuestList)
+        //{
+        //    Debug.Log(a.code);
+        //    Debug.Log(a.icon);
+        //    Debug.Log(a.category);
+        //    Debug.Log(a.food.Count);
+        //    Debug.Log(a.food[0]);
+        //    Debug.Log(a.food[1]);
+        //    Debug.Log(a.food[2]);
+        //    Debug.Log(a.ani_eat01.Count);
+        //    Debug.Log("----------------");
+        //}
         
     }
 
